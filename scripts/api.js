66e6f394d21e1b333427571f7bf71a24c58714f0
ssheaks <sarah.sheaks@gmail.com>
2018-02-28 09:19:01 -0800

    add add item to store and fix js files

--- scripts/api.js
+++ scripts/api.js
@@ -1,24 +1,24 @@
 'use strict';
 const api = function() {
-    const BASE_URL = 'https://thinkful-list-api.herokuapp.com/parker'
-    const getItems = function(callback){
-        $.getJSON(`${BASE_URL}/items`, callback);
-    }
-    const createItem = function(name, callback){
-        const newItem = JSON.stringify({
-            name,
-        })
-        $.ajax({
-        url: `${BASE_URL}/items`,
-        method: 'POST',
-        contentType: 'application/json',
-        data: newItem,
-        success: callback,
-    })
-}
-    return {
-        getItems,
-        createItem,
-    }
+  const BASE_URL = 'https://thinkful-list-api.herokuapp.com/parker';
+  const getItems = function(callback){
+    $.getJSON(`${BASE_URL}/items`, callback);
+  };
+  const createItem = function(name, callback){
+    const newItem = JSON.stringify({
+      name,
+    });
+    $.ajax({
+      url: `${BASE_URL}/items`,
+      method: 'POST',
+      contentType: 'application/json',
+      data: newItem,
+      success: callback,
+    });
+  };
+  return {
+    getItems,
+    createItem,
+  };
 }();
 
